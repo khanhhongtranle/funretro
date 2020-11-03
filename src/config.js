93f89bb81801a78862ebb866ebe5b58705f28769
khanhhongtranle <khanhhong141049@gmail.com>
2020-11-03 18:11:45 +0700

    build

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    "api_url": "http://localhost:8080/funretro/api/",//must end with slash (/),
+    "api_url": "http://52.77.203.212//api/",//must end with slash (/),
     "cookie_name": "funretro-react",
     "cookie_user_id": "funretro-user-id",
 };
