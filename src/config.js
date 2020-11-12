8418121dbd517fe08fd651640cd2bdb1db3787c6
khanhhongtranle <khanhhong141049@gmail.com>
2020-11-12 18:02:05 +0700

    share board

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    "api_url": "http://localhost/khanhhongtranle/funretro/api/",//must end with slash (/),
-    "cookie_name": "funretro-react",
-    "cookie_user_id": "funretro-user-id",
-    "google_client_id":"817492327547-f0qhi4ed61vsjua0vaj3vgtpqnlvap68.apps.googleusercontent.com"
-};
