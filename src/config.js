2588477a2582e65477857f53695d747466665e03
Aramefiko <admin@aramefiko.com>
2020-11-12 15:47:52 +0700

    login google

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    "api_url": "https://khanhhong.aramefiko.com/api/",//must end with slash (/),
-    "cookie_name": "funretro-react",
-    "cookie_user_id": "funretro-user-id",
-    "google_client_id":"817492327547-f0qhi4ed61vsjua0vaj3vgtpqnlvap68.apps.googleusercontent.com"
-};
