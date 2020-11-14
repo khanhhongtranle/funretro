bac6fa4d33b93bc4ecfd5e157e55688ef8343f98
Aramefiko <admin@aramefiko.com>
2020-11-14 14:42:49 +0700

    update login

--- src/components/login.js
+++ src/components/login.js
@@ -27,41 +27,49 @@ function Login() {
     }
 
     const responseGoogle = (response) => {
-        console.log(response);
-        const params = new FormData();
-        params.append('email', response.tt.$t);
-        params.append('first_name', response.tt.gV);
-        params.append('last_name', response.tt.jT);
-        params.append('username', response.tt.CT);
+        if (response.hasOwnProperty('$t')) {
+            const params = new FormData();
+            params.append('email', response.tt.$t);
+            params.append('first_name', response.tt.gV);
+            params.append('last_name', response.tt.jT);
+            params.append('username', response.tt.CT);
 
-        callAPI('loginGoogle', params, function (res) {
-            if (res.success) {
-                setCookie(config.cookie_name, res.token);
-                setCookie(config.cookie_user_id, res.user_id);
-                window.location.href = "/";
-            } else {
-                alert('Login failed');
-            }
-        });
+            callAPI('loginGoogle', params, function (res) {
+                if (res.success) {
+                    setCookie(config.cookie_name, res.token);
+                    setCookie(config.cookie_user_id, res.user_id);
+                    window.location.href = "/";
+                } else {
+                    alert('Login failed');
+                }
+            });
+        }
     }
 
     const responseFacebook = (response) => {
-        console.log(response);
-        const params = new FormData();
-        params.append('username', response.userID);
-        params.append('email', response.email);
-        params.append('first_name', response.name);
-        params.append('last_name', '');
-        callAPI('loginFacebook', params, function (res) {
-            if (res.success) {
-                setCookie(config.cookie_name, res.token);
-                setCookie(config.cookie_user_id, res.user_id);
-                window.location.href = "/";
-            } else {
-                alert('Login failed');
-            }
-        });
+        if (response.hasOwnProperty()) {
+            const params = new FormData();
+            params.append('username', response.id);
+            params.append('email', response.email);
+            params.append('first_name', response.name);
+            params.append('last_name', '');
+            callAPI('loginFacebook', params, function (res) {
+                if (res.success) {
+                    setCookie(config.cookie_name, res.token);
+                    setCookie(config.cookie_user_id, res.user_id);
+                    window.location.href = "/";
+                } else {
+                    alert('Login failed');
+                }
+            });
+        }
     }
+
+    const handleKeyPress = (event) => {
+        if (event.code === "Enter") {
+            actionLogin();
+        }
+    };
 
     return (
         <Container style={{paddingTop: "2rem"}}>
@@ -70,44 +78,46 @@ function Login() {
                 <Form>
                     <Form.Group controlId="username">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)}/>
+                        <Form.Control onKeyPress={handleKeyPress} type="text" placeholder="Username" onChange={e => setUsername(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group controlId="password">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                        <Form.Control onKeyPress={handleKeyPress} type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                     </Form.Group>
 
                     <Row>
                         <Col>
-                            <Button onClick={actionLogin} variant="primary" type="button">Login</Button>
+                            <Button onClick={actionLogin} variant="success" type="button">Login</Button>
                         </Col>
-                        <Col>
+                        <Col style={{textAlign:"right"}}>
                             <Link style={{marginLeft: "20px", marginRight: "20px"}} to="/signup">Sign up</Link>
                         </Col>
                     </Row>
 
-                    <Row>
+                    <Row style={{marginTop:"1rem", textAlign:"center"}}>
                         <Col>
                             <GoogleLogin
+                                className={"btn btn-outline-primary"}
+                                style={{textAlign:"center",width:"300px"}}
                                 clientId={config.google_client_id}
                                 buttonText="Login"
                                 onSuccess={responseGoogle}
-                                onFailure={responseGoogle}
                                 cookiePolicy={'single_host_origin'}
                             />
                         </Col>
                     </Row>
 
-                    <Row>
+                    <Row style={{marginTop:"1rem", textAlign:"center"}}>
                         <Col>
                             <FacebookLogin
-                                appId="403062567533270"
-                                autoLoad={false}
+                                className={"btn btn-outline-primary"}
+                                style={{textAlign:"center",width:"300px"}}
+                                appId={config.facebook_app_id}
+                                autoLoad={true}
                                 textButton="Login"
                                 fields="name,email,picture"
-                                onClick={()=>{console.log("Clicked!")}}
-                                callback={responseFacebook} />
+                                callback={responseFacebook}/>
                         </Col>
                     </Row>
                 </Form>
